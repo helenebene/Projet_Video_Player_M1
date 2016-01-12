fd0f17ce5dd934a952bddac80cdc1d96b3111313
heleneprojet <helene.mazars@isen-lille.fr>
2016-01-12 13:54:02 +0100

    Initial commit

---
+++ app/src/main/java/com/skholingua/android/custom_ratingbar/MainActivity.java
@@ -0,0 +1,258 @@
+package com.skholingua.android.custom_ratingbar;
+
+import android.app.Activity;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+
+public class MainActivity extends Activity implements SeekBar.OnSeekBarChangeListener {
+
+
+    private ImageButton next, previous, stopMedia, playPause;
+    private MediaPlayer mediaPlayer;
+    private double startTime = 0;
+    private double finalTime = 0;
+    private Handler myHandler = new Handler();
+
+    private int forwardTime = 5000;
+    private int backwardTime = 5000;
+    private SeekBar seekbar;
+
+    private TextView time, allTime, songName;
+    private int etat;
+
+
+    AudioManager audioManager;
+
+    public static int oneTimeOnly = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+        etat = 0;
+
+        next = (ImageButton) findViewById(R.id.next);
+        previous = (ImageButton) findViewById(R.id.previous);
+        playPause = (ImageButton) findViewById(R.id.playpause);
+        playPause.setBackgroundResource(R.drawable.playtopause);
+        stopMedia = (ImageButton) findViewById(R.id.stopmedia);
+
+        time = (TextView) findViewById(R.id.time);
+        allTime = (TextView) findViewById(R.id.alltime);
+        songName = (TextView) findViewById(R.id.songname);
+        songName.setText("Song.mp3");
+
+        //Code pour la gestion du volume
+        audioManager = (AudioManager) getSystemService(getApplicationContext().AUDIO_SERVICE);
+        int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        SeekBar volControl = (SeekBar) findViewById(R.id.volbar);
+        volControl.setMax(maxVolume);
+        volControl.setProgress(curVolume);
+        volControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+            @Override
+            public void onStopTrackingTouch(SeekBar arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar arg0) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+                // TODO Auto-generated method stub
+                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, arg1, 0);
+            }
+        });
+
+        // Selection de la musique
+        mediaPlayer = MediaPlayer.create(this, R.raw.alex);
+
+
+        seekbar = (SeekBar) findViewById(R.id.seekBar);
+        seekbar.setOnSeekBarChangeListener(this);
+
+        stopMedia.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mediaPlayer.isPlaying() || mediaPlayer.getDuration() > 0) {
+                    mediaPlayer.stop();
+
+                    try {
+                        mediaPlayer.prepare();
+                    } catch (IllegalStateException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                    mediaPlayer.seekTo(0);
+                    etat=0;
+                    seekbar.setProgress(0);
+                }
+            }
+        });
+
+
+        playPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (etat == 0) {
+                    playPause.setBackgroundResource(R.drawable.pausetoplay);
+                    Toast.makeText(getApplicationContext(), "Playing sound", Toast.LENGTH_SHORT).show();
+                    mediaPlayer.start();
+
+                    finalTime = mediaPlayer.getDuration();
+                    startTime = mediaPlayer.getCurrentPosition();
+
+                    if (oneTimeOnly == 0) {
+                        seekbar.setMax((int) finalTime);
+                        oneTimeOnly = 1;
+                    }
+                    allTime.setText(String.format("%d min, %d sec",
+                                    TimeUnit.MILLISECONDS.toMinutes((long) finalTime),
+                                    TimeUnit.MILLISECONDS.toSeconds((long) finalTime) -
+                                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) finalTime)))
+                    );
+
+                    time.setText(String.format("%d min, %d sec",
+                                    TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                                    TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                                            TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes((long) startTime)))
+                    );
+
+                    seekbar.setProgress((int) startTime);
+                    myHandler.postDelayed(UpdateSongTime, 100);
+                    etat = 1;
+                } else {
+                    playPause.setBackgroundResource(R.drawable.playtopause);
+                    Toast.makeText(getApplicationContext(), "Pausing sound", Toast.LENGTH_SHORT).show();
+                    mediaPlayer.pause();
+                    etat = 0;
+                }
+
+            }
+        });
+
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int temp = (int) startTime;
+
+                if ((temp + forwardTime) <= finalTime) {
+                    startTime = startTime + forwardTime;
+                    mediaPlayer.seekTo((int) startTime);
+                    Toast.makeText(getApplicationContext(), "You have Jumped forward 5 seconds", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Cannot jump forward 5 seconds", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        previous.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int temp = (int) startTime;
+
+                if ((temp - backwardTime) > 0) {
+                    startTime = startTime - backwardTime;
+                    mediaPlayer.seekTo((int) startTime);
+                    Toast.makeText(getApplicationContext(), "You have Jumped backward 5 seconds", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Cannot jump backward 5 seconds", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private Runnable UpdateSongTime = new Runnable() {
+        public void run() {
+            startTime = mediaPlayer.getCurrentPosition();
+            time.setText(String.format("%d min, %d sec",
+
+                            TimeUnit.MILLISECONDS.toMinutes((long) startTime),
+                            TimeUnit.MILLISECONDS.toSeconds((long) startTime) -
+                                    TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.
+                                            toMinutes((long) startTime)))
+            );
+            seekbar.setProgress((int) startTime);
+            myHandler.postDelayed(this, 100);
+        }
+    };
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        try {
+            if (mediaPlayer.isPlaying() || mediaPlayer != null) {
+                if (fromUser)
+                    mediaPlayer.seekTo(progress);
+            } else if (mediaPlayer == null) {
+                Toast.makeText(getApplicationContext(), "Media is not running",
+                        Toast.LENGTH_SHORT).show();
+                seekBar.setProgress(0);
+            }
+        } catch (Exception e) {
+            Log.e("seek bar", "" + e);
+            seekBar.setEnabled(false);
+
+        }
+    }
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        // TODO Auto-generated method stub
+
+    }
+}
